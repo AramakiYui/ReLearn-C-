34c1a45fff846f0b682a85ceb203dc87d5830c94
imwayfinder <35069641+imwayfinder@users.noreply.github.com>
2019-03-31 13:36:15 +0800

    Add files via upload

---
+++ 018抽奖结构.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <conio.h>
+using namespace std;
+
+//方法一:time(NULL)
+// int rand_number()
+// {
+// 	return time(NULL);
+// }
+
+//方法二:rand()
+int rand_number()
+{
+    return rand();
+}
+
+int main()
+{
+	srand(time(NULL));	//更快的滚动
+	int i;
+	while(true)
+	{
+	 	system("cls");
+	    string name[3]={"王小二","张小三","李小四"};
+	    i = rand_number()%3;
+	    cout<<"the person is:"<<name[i]<<endl;
+		if(kbhit())	//按下键盘停止
+			break;
+	}
+	system pause("pause");
+
+    return 0;
+}
